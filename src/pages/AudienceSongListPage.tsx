ee4169c349c2de6817a765850b024c2cd4803743
facerusl-eng <facerusl@gmail.com>
2026-05-12 12:23:37 +0200

    Use Harald Live artwork for audience playlist cover

--- src/pages/AudienceSongListPage.tsx
+++ src/pages/AudienceSongListPage.tsx
@@ -786,7 +786,7 @@ function AudienceSongListPage() {
             className="audience-playlist-choice audience-playlist-choice-jukebox"
             onClick={() => setActiveSetlist('human_jukebox')}
           >
-            <img src="/images/playlist-karaoke.jpg" alt="Human Jukebox playlist cover" className="audience-playlist-choice-cover audience-playlist-choice-cover-jukebox" />
+            <img src="/images/Harald%20Live.png" alt="Harald Live playlist cover" className="audience-playlist-choice-cover audience-playlist-choice-cover-jukebox" />
             <div className="audience-playlist-choice-overlay">
               <strong className="audience-playlist-choice-title">Human Jukebox</strong>
               <span className="audience-playlist-choice-sub">{copy.hostPlays}</span>
@@ -798,7 +798,7 @@ function AudienceSongListPage() {
             className="audience-playlist-choice audience-playlist-choice-karaoke"
             onClick={() => setActiveSetlist('karaoke')}
           >
-            <img src="/images/playlist-human-jukebox.jpg" alt="Karaoke playlist cover" className="audience-playlist-choice-cover" />
+            <img src="/images/playlist-karaoke.jpg" alt="Karaoke playlist cover" className="audience-playlist-choice-cover" />
             <div className="audience-playlist-choice-overlay">
               <strong className="audience-playlist-choice-title">Karaoke</strong>
               <span className="audience-playlist-choice-sub">{copy.youSing}</span>
